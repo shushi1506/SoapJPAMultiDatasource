1e71e9119b1ed43f4accb354c7e92ba02ac6c674
Bui Tuan Anh <nhomavidai@gmail.com>
2018-07-11 09:29:14 +0700

    Update DefinedConfig.java

--- webserviceqlcb/src/main/java/com/soap/qlcb/DefinedConfig.java
+++ webserviceqlcb/src/main/java/com/soap/qlcb/DefinedConfig.java
@@ -7,6 +7,6 @@ package com.soap.qlcb;
 public class DefinedConfig {
     public static final String NAME_SPACE="http://basic.soap";
     public static final String DB_ORACLE_QLCB_URL = "jdbc:oracle:thin:@qltb.api.qlcb:1521:dbtd";
-    public static final String DB_ORACLE_QLCB_USER = "qlcb2";
-    public static final String DB_ORACLE_QLCB_PASS = "qlcb";
+    public static final String DB_ORACLE_QLCB_USER = ;
+    public static final String DB_ORACLE_QLCB_PASS = ;
 }
